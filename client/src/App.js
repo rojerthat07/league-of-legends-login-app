29e0a2c86dfcc0064f236046cfbbdab61cde05e2
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-08-05 14:15:15 +0800

    login and successfully register page creation, form validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import './main.scss';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./main.scss";
 
 //Components
-import Register from './components/Register';
-import Login from './components/Login';
-import Navigation from './components/Navigation';
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Navigation from "./components/Navigation";
+import SuccessfulRegister from "./components/SuccessfulRegister";
 
 function App() {
   return (
     <div className="App">
-      
-    <Router>
-      <Switch>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <Navigation></Navigation>
+          </Route>
 
-      <Route path="/" exact>
-        <Navigation></Navigation>
-      </Route>
+          <Route path="/login" component={Login}></Route>
+          <Route path="/register" component={Register}></Route>
 
-      <Route path="/login" component={Login}></Route>
-      <Route path="/register" component={Register}></Route>
-
-      </Switch>
-    </Router>
+          <Route
+            path="/successfulregistration"
+            component={SuccessfulRegister}
+          ></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
