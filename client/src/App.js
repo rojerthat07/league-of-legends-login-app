6a6162f408ffdbb9d38d1aa1d45b2c842740d06e
Jer Carlo Catallo <jercarlopogi07@gmail.com>
2020-08-05 10:05:06 +0800

    creating router and creating login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import './main.scss';
+
+//Components
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       
+    <Router>
+      <Switch>
+
+      <Route path="/" exact>
+      <h1>LEAGUE OF LEGENDS</h1>
+      </Route>
+
+      <Route path="/login" component={Login}></Route>
+      <Route path="/register" component={Register}></Route>
+
+      </Switch>
+    </Router>
     </div>
   );
 }
